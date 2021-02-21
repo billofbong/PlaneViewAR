59aaa19b882396f03efce782c747c2942330fa8c
MrEpicSquare <corbinlee98@gmail.com>
2021-02-21 04:48:48 -0800

    added basic rotation

--- Assets/MovePlane.cs
+++ Assets/MovePlane.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class MovePlane : MonoBehaviour
 {
+    // The target to travel towards.
+    public Transform target;
+
+    // The velocity of the plane.
+    public float velocity = 39f;
+
+    // The rate of how much the plane will turn in any direction per second in degrees.
+    public float rateOfTurn = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +20,26 @@ public class MovePlane : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position += transform.forward * 39 * Time.deltaTime;
+        float yRotation = transform.rotation.eulerAngles.y;
+        float angleTarget;
+        if (target == null)
+            angleTarget = StaticPlaneInfo.staticAircraft.true_track;
+        else
+            angleTarget = target.rotation.eulerAngles.y;
+
+        if (Mathf.Abs(yRotation - angleTarget) < Time.deltaTime)
+        {
+            transform.rotation = Quaternion.Euler(0, angleTarget, 0);
+        }
+        else if (yRotation > angleTarget)
+        {
+            transform.Rotate(0, -rateOfTurn * Time.deltaTime, 0);
+        }
+        else
+        {
+            transform.Rotate(0, rateOfTurn * Time.deltaTime, 0);
+        }
     }
 }

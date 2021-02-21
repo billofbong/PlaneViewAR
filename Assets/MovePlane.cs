5b15da4dfb0233d4fcfce14f0b80ec8e7c39fafd
Will Wright <will@wright.lv>
2021-02-20 21:33:04 -0800

    Added forward script for testing

--- Assets/MovePlane.cs
+++ Assets/MovePlane.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrigTransform : MonoBehaviour
+public class MovePlane : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float strength = 1;
-
     void Start()
     {
+        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float Z_POS = transform.position.z;
-        transform.Translate(0, 0, strength * Mathf.Cos(Time.time));
+        transform.position += transform.forward * 39 * Time.deltaTime;
     }
 }

6ee1601cffd1ed5a98ef28d89d30a68cb80992b2
MrEpicSquare <corbinlee98@gmail.com>
2021-02-21 03:50:23 -0800

    Added StaticPlaneInfo

---
+++ Assets/Scripts/StaticPlaneInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static OpenSky;
+
+public static class StaticPlaneInfo
+{
+    public static Aircraft staticAircraft { get; set; }
+}
